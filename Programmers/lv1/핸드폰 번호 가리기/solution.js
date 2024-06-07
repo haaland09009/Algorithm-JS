90687a50a7c7e0966afa0c7fbc81ca05e1164351
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-06-07 13:13:19 +0900

    20240607

--- Programmers/lv1/핸드폰 번호 가리기/solution.js
+++ Programmers/lv1/핸드폰 번호 가리기/solution.js
@@ -3,3 +3,9 @@ function solution(phone_number) {
   
     return '*'.repeat(phone_number.length-4) + phone_number.slice(-4);
 }
+
+/*
+function solution(phone_number) {
+    return '*'.repeat(phone_number.length-4) + phone_number.slice(phone_number.length-4, phone_number.length);
+}
+*/
