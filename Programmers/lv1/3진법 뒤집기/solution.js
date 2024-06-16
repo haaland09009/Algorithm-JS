76085a3448e537939e01f7efd536868119245b7a
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-06-16 18:51:37 +0900

    20240616

--- Programmers/lv1/3진법 뒤집기/solution.js
+++ Programmers/lv1/3진법 뒤집기/solution.js
@@ -24,3 +24,31 @@ function convertNumber(n) {
     
     return str;
 }
+
+
+/*
+function solution(n) {
+    var answer = 0;
+    
+    var letter = convert(n);
+    
+    for (var i=0; i<letter.length; i++) {
+        answer += Math.pow(3, letter.length - 1 - i) * Number(letter.charAt(i));
+    }
+    
+    return answer;
+}
+
+
+function convert(n) {
+    
+    var str = "";
+    
+    while (n > 0) {
+        str = String(n % 3) + str;
+        n = Math.floor(n / 3);
+    }
+    
+    return str.split('').reverse().join('');
+}
+*/
