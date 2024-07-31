f4017cd69d75e22dc8423c62ddcba607347611c1
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-08-01 00:15:00 +0900

    20240801

--- Programmers/lv1/두 정수 사이의 합/solution.js
+++ Programmers/lv1/두 정수 사이의 합/solution.js
@@ -2,6 +2,21 @@
 function solution(a, b) {
     var answer = 0;
     
+    var left = (a <= b) ? a : b;
+    var right = (a == left) ? b : a;
+    
+    for (var i=left; i<=right; i++) {
+        answer += i;
+    }
+    
+    
+    return answer;
+}
+
+/*
+function solution(a, b) {
+    var answer = 0;
+    
     var a1 = Math.min(a, b);
     var a2 = Math.max(a, b);
     
@@ -10,3 +25,4 @@ function solution(a, b) {
     }
     return answer;
 }
+*/
