4504c6bd3bb70e6c255c4f162df70ad70e2dab7d
09^_^ <87968927+haaland09009@users.noreply.github.com>
2024-08-05 00:11:39 +0900

    20240805

--- Programmers/lv1/콜라츠 추측/solution.js
+++ Programmers/lv1/콜라츠 추측/solution.js
@@ -17,3 +17,26 @@ function solution(num) {
     
     return answer;
 }
+/*
+function solution(num) {
+    var answer = 0;
+    
+    if (num == 1) {
+        return 0;
+    }
+    
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else {
+            num = num * 3 + 1;
+        }
+        answer++;
+        if (answer == 500) {
+            return -1;
+        }
+    }
+    
+    return answer;
+}
+*/
